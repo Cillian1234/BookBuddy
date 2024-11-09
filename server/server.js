dfb10fb0a675e7bb135176e3bf5670ab113c85e2
Cillian <cillsm80@gmail.com>
2024-11-09 05:44:50 +0000

    Init

--- server/server.js
+++ server/server.js
@@ -10,6 +10,6 @@ app.use(express.json());
 app.use("/record", records);
 
 // Start server
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server started on port ${PORT}`);
 })
