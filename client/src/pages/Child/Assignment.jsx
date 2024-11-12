5e1976803d6be531ba5ed186eed3cffb24cf86c9
ajjane18 <B00154235@mytudublin.ie>
2024-11-12 17:05:26 +0000

    coding the child pages
    Not much was changed just changed, background, fixed navbar for child(not complete but got it working at least..).

--- client/src/pages/Child/Assignment.jsx
+++ client/src/pages/Child/Assignment.jsx
@@ -1,12 +1,18 @@
 import {Link} from 'react-router-dom';
 //Importing the navbar
 import Navbar from '../../../src/components/Navbar.jsx';
+//Importing images for navbar
+//Importing the images used to link to their page
+import starRevB from '../../assets/Images by AJ/starRevB.png';
+import home from '../../assets/Images by AJ/home.png';
+//Importing the css files from css folder
+import './css/assign.css'; 
 
 export default function Assignment() {
   return (
     <> 
     <ChildNavbar />
-    <div>
+    <div className = "assignCon">
       <h1>Assignment Page</h1>
       <p>This is where assignments will be displayed and managed.</p>
     </div>
@@ -16,9 +22,15 @@ export default function Assignment() {
 
 function ChildNavbar() {
   return (
+    <>
+    <Navbar/>
+
       <nav className="ChildNB">
-          <Navbar />
-          <Link to={"/Child/Review"}>Review</Link>
+          <div className="navBTNS">
+          <Link to={"/Child"}><img id = {"home"} src={home} alt={"home"}/></Link>
+          <Link to={"/Child/Review"}><img id = {"strB"} src={starRevB} alt={"Review"}/></Link>
+          </div>
       </nav>
+    </>
   )
 }
